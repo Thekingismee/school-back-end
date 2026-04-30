7f07af73e2a620ec0860c9163d1b6a01aa30aab1
y.abdelillah <yahiaabdelillah360@gmail.com>
2026-04-30 09:36:45 +0100

    fin backend

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['GS l\'Atome' => app()->version()];
 });
 
 // Route::post('/inscriptions', [InscriptionController::class, 'store']);
